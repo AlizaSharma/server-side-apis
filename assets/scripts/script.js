3c1341d74f7c669e4653bb3bea415080f28fa314
Aliza Sharma <aliza.sharma12@gmail.com>
2023-07-13 15:43:41 -0500

    updated current date

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,7 +5,7 @@ var tempEl =document.querySelector(".card-text");
 var humidityEl =document.querySelector(".card-text-two");
 var windEl =document.querySelector(".card-text-three");
 var cityEl =document.querySelector(".cardTodayCityName");
-
+var dateEl =document.querySelector(".cardTodayDate");
 
 // take input and plug it into api query 
 var apiKey = "894a8632519c8ba9df1569e892b67fd0";
@@ -39,10 +39,14 @@ async function getCurrentWeather(lat,lon){
       var windSpeed = data.list[0].wind.speed
       var humidity = data.list[0].main.humidity
       var icon = data.list[0].weather[0].icon
+      var date = data.list[0].dt_txt
+
       
     
       var weatherImg = document.querySelector(".icon-box");
       weatherImg.innerHTML = "<img" + " src=" + "http://openweathermap.org/img/wn/" + icon + "@2x.png>";
+
+      dateEl.innerHTML = date 
 
       
     tempEl.innerHTML = temp + " \xB0" + "F";
@@ -52,12 +56,6 @@ async function getCurrentWeather(lat,lon){
     windEl.innerHTML = windSpeed + " mph";
 
     cityEl.innerHTML = cityName;
-
-
-
-
-
-
 
      
     })
